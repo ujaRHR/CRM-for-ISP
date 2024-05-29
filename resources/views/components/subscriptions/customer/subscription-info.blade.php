c113ad048b497d19b22b4732a8bc5dd08b79715c
ujaRHR <rhraju.dev@gmail.com>
2024-05-29 22:41:09 +0600

    Created: Customer Subs. Frontend

---
+++ resources/views/components/subscriptions/customer/subscription-info.blade.php
@@ -0,0 +1,78 @@
+<div class="container py-3">
+  <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+    <h2 class="display-5 fw-normal">Pricing</h2>
+    <p class="fs-5 text-muted">Explore our variety of pricing plans designed to fit different needs and budgets. Whether you need internet for personal use or for your business, we have a plan that's right for you.</p>
+  </div>
+
+  <div class="row text-center">
+    <div class="col-md-3">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-bold">Silver</h4>
+        </div>
+        <div class="card-body">
+          <h3 class="card-title pricing-card-title">৳650<small class="text-muted fw-light">/month</small></h3>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Speed: 15 MBPS</li>
+            <li>Uptime: 99.9%</li>
+            <li>Limit: 150 GB per month</li>
+            <li class="text-decoration-line-through">Ideal For: Gaming, Streaming</li>
+          </ul>
+          <button type="button" class="btn btn-success">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-bold">Gold</h4>
+        </div>
+        <div class="card-body">
+          <h3 class="card-title pricing-card-title">৳850<small class="text-muted fw-light">/month</small></h3>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Speed: 25 MBPS</li>
+            <li>Uptime: 99.9%</li>
+            <li>Limit: 230 GB per month</li>
+            <li class="text-decoration-line-through">Ideal For: Gaming, Streaming</li>
+          </ul>
+          <button type="button" class="btn btn-success">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-bold">Platinum</h4>
+        </div>
+        <div class="card-body">
+          <h3 class="card-title pricing-card-title">৳1050<small class="text-muted fw-light">/month</small></h3>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Speed: 40 MBPS</li>
+            <li>Uptime: 99.9%</li>
+            <li>Limit: 300 GB per month</li>
+            <li>Ideal For: Gaming, Streaming</li>
+          </ul>
+          <button type="button" class="btn btn-success">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-bold">Corporate</h4>
+        </div>
+        <div class="card-body">
+          <h3 class="card-title pricing-card-title">৳1150<small class="text-muted fw-light">/month</small></h3>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Speed: 45 MBPS</li>
+            <li>Uptime: 99.9%</li>
+            <li>Limit: 500 GB per month</li>
+            <li>Ideal For: Conference, Streaming</li>
+          </ul>
+          <button type="button" class="btn btn-success">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
