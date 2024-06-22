9b7a08c1085e065a264a699754d90e864f5d6862
ujaRHR <rhraju.dev@gmail.com>
2024-06-22 17:27:52 +0600

    Bug: JWT Issue

--- app/Http/Middleware/TokenVerificationMiddleware.php
+++ app/Http/Middleware/TokenVerificationMiddleware.php
@@ -10,7 +10,7 @@ use Firebase\JWT\ExpiredException;
 use Firebase\JWT\Key;
 use Firebase\JWT\JWT;
 use Exception;
-use Nette\Schema\Expect;
+
 
 class TokenVerificationMiddleware
 {
@@ -21,20 +21,13 @@ class TokenVerificationMiddleware
             $decoded_token = JWTToken::verifyToken($request->cookie('token'));
 
             if ($decoded_token == ('unauthorized' || 'expired')) {
-                return redirect('/admin-error');
+                return redirect('/customer-login');
             } else {
                 $request->headers->set('id', $decoded_token->id);
                 $request->headers->set('email', $decoded_token->email);
                 $request->headers->set('type', $decoded_token->type);
 
                 return $next($request);
-            }
-        } catch (ExpiredException $e) {
-            $decoded_token = JWT::decode($request->cookie('token'), new Key(env('JWT_SECRET'), 'HS256'), 'JWT', 'HS256', false);
-            if (isset($decoded_token->type) && $decoded_token->type == 'admin') {
-                return redirect('/admin-login');
-            } else {
-                return redirect('/customer-login');
             }
         } catch (Exception $e) {
             return redirect('/customer-login');
